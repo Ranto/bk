69a7b723c79c6d450d4196efc246a97f56bf2790
Ranto Ambinintsoa ANDRIANJAFY <andrianjafy@gmail.com>
2018-03-01 11:12:18 +0300

    ajout toaster

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,23 +2,26 @@ import { SurveyModule } from './survey/survey.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ToasterModule } from 'angular2-toaster';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SurveyModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MainComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SurveyModule,
+        ToasterModule.forRoot()
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
